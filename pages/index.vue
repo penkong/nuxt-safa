1b0d5b8cd7124fe28ddbf21cfd5ebcf5de1a5a48
TMP <tmp@safarayaneh.local>
2019-08-10 09:15:56 +0430

    add up files from vue app

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,33 @@
+/* eslint-disable */
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-safa
-      </h1>
-      <h2 class="subtitle">
-        My luminous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="home">
+    <BigCard style="margin-top: 5.4rem;" />
+    <div class="card-container">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+// @ is an alias to /src
+import Card from "../../components/Card/Card";
+import BigCard from "../../components/BigCard/BigCard";
 
 export default {
+  name: "Home",
   components: {
-    Logo
+    Card,
+    BigCard
   }
-}
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style scoped lang="scss">
+@import "../../sass/abstract/mixins";
+@import "../../sass/pages/Home";
 </style>
